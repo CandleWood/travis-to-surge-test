97eb707013c4f191a9baf30dd4251f7eddd11f90
allejo <vladimir.jimenez.102@my.csun.edu>
2017-02-22 09:14:02 -0800

    Fix quotes in variables

--- .travis/surge-deploy.sh
+++ .travis/surge-deploy.sh
@@ -4,9 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 URL="https://allejo-surge_test-$TRAVIS_PULL_REQUEST.surge.sh"
 if [ ! -f /usr/local/bin/surge ]; then npm install -g surge; fi
-surge --project ./_site --domain "$URL"
+surge --project ./_site --domain "$URL" &> /dev/null
 SURGE_STATUS=$?
 STATUS=$([ $SURGE_STATUS -eq 0 ] && echo "success" || echo "error")
 DESC=$([ $SURGE_STATUS -eq 0 ] && echo "Staging website deployed successfully" || echo "Staging website deployed failed")
 
-bash $DIR/pr_status.sh $STATUS $DESC $URL
+bash $DIR/pr_status.sh "$STATUS" "$DESC" "$URL"
