802d12c4718142eb519eb1b1bd39dc8cf7d1bdfa
Vladimir Jimenez <allejo@users.noreply.github.com>
2017-02-22 16:24:32 -0800

    Update surge-deploy.sh

--- .travis/surge-deploy.sh
+++ .travis/surge-deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
 URL="https://allejo-surge_test-$TRAVIS_PULL_REQUEST.surge.sh"
 if [ ! -f /usr/local/bin/surge ]; then npm install -g surge; fi
 surge --project ./_site --domain "$URL" &> /dev/null
@@ -9,4 +7,5 @@ SURGE_STATUS=$?
 STATUS=$([ $SURGE_STATUS -eq 0 ] && echo "success" || echo "error")
 DESC=$([ $SURGE_STATUS -eq 0 ] && echo "Staging website deployed successfully" || echo "Staging website deployed failed")
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 bash $DIR/pr_status.sh "$STATUS" "$DESC" "$URL"
